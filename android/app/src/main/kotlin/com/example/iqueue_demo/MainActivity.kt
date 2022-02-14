b33c246b61894c96a7383a00dc4afa937fb5b808
badalsura <93517425+badalsura@users.noreply.github.com>
2022-02-14 19:12:57 +0530

    first_commit

---
+++ android/app/src/main/kotlin/com/example/iqueue_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.iqueue_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  //MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('iQueue 365'),
+        ),
+        body: Text('test'),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.reduce_capacity_outlined),
+              label: 'iQueue',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+              //backgroundColor: Colors.blueAccent,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favourite',
+              //backgroundColor: Colors.blueAccent,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.lightbulb_outline),
+              label: 'Plans',
+              //backgroundColor: Colors.grey,
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.indigo,
+          unselectedItemColor: Colors.grey,
+          backgroundColor: Colors.white,
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+}
